f8394d9ced15f6cb80c3b55b2eb3903d3386507c
Jordann120 <75611870+Jordann120@users.noreply.github.com>
2025-04-17 14:16:36 +0200

    init project

---
+++ ModernSoftwareTest/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using ModernSoftwareApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSoftwareTest
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var calc = new Calculator();
+            Assert.Equal(5, calc.Add(2, 3));
+        }
+
+    }
+}
